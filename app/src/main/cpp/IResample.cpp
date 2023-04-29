1c33896a78e35f587bcbe81745a3e61bd6808b9f
Fengbao <Fengbao@atguigu.com>
2023-04-29 23:36:17 +0800

    first commit

---
+++ app/src/main/cpp/IResample.cpp
@@ -0,0 +1,13 @@
+#include "IResample.h"
+#include "XLog.h"
+
+void IResample::Update(XData data)
+{
+
+    XData d = this->Resample(data);
+    //XLOGE("this->Resample(data) %d",d.pts);
+    if(d.size > 0)
+    {
+        this->Notify(d);
+    }
+}
---
+++ app/src/main/cpp/IVideoView.cpp
@@ -0,0 +1,8 @@
+#include "IVideoView.h"
+#include "XLog.h"
+
+void IVideoView::Update(XData data)
+{
+    //("IVideoView->Update(data) %d",data.pts);
+    this->Render(data);
+}
